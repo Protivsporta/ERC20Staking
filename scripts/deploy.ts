445cc6637b6eb1fbe5311fb074a5cf538994303d
Alexander Bushukin <46033931+Protivsporta@users.noreply.github.com>
2022-03-25 20:41:11 +0300

    Updated deploy script for constructor logic

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,7 +3,7 @@ import { ethers } from "hardhat";
 async function main() {
   const [signer] = await ethers.getSigners();
   const Staking = await ethers.getContractFactory("Staking", signer);
-  const staking = await Staking.deploy();
+  const staking = await Staking.deploy("0x098DaDA18eb6790e2cEe0575BB89485Be066c372", "0x17E117Ed9929Ed8e37B369c87dE1613377Ca07c6", 20, 600, 600);
 
   await staking.deployed();
 
